805ddf9f1f0e659666044d20768b2d815735c5a9
Derpidius <derpidiusbidoof@gmail.com>
2024-07-14 15:28:19 -0500

    And we move!!!
    Walking movement works! And also crouching is there and working

--- BootCamp_FPS-Controller/Assets/_Scripts/PlayerMovement.cs
+++ BootCamp_FPS-Controller/Assets/_Scripts/PlayerMovement.cs
@@ -29,7 +29,14 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        float foward = inputManager.inputMaster.Movement.Forward.ReadValue<float>();
+        float right = inputManager.inputMaster.Movement.Right.ReadValue<float>();
+        Vector3 move = transform.right * right + transform.forward * foward;
+
+        move *= inputManager.inputMaster.Movement.Run.ReadValue<float>() == 0 ? speed : runSpeed;
+        transform.localScale = new Vector3(1, inputManager.inputMaster.Movement.Crouch.ReadValue<float>() == 0 ? 1f : 0.72618f, 1);
+
+        rb.velocity = new Vector3 (move.x, rb.velocity.y, move.z);
     }
 
     private void OnCollisionEnter(Collision collision)
